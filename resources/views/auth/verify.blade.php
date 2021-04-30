7bc44dca181a675891e1e502b9c55bf5a8d03b84
jafari <momeneh.jafari@gmail.com>
2021-04-30 15:38:53 +0430

    second commit

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -1,30 +1,30 @@
-@extends('layouts.app', ['class' => 'bg-default'])
+@extends('layouts.app', ['pageSlug' => ''])
 
 @section('content')
-    @include('layouts.headers.guest')
+{{--    @include('layouts.headers.guest')--}}
 
-    <div class="container mt--8 pb-5">
+    <div class="container mt--8 pb-5 verify">
         <div class="row justify-content-center">
-            <div class="col-lg-5 col-md-7">
+            <div class="col-lg-6 col-md-7">
                 <div class="card bg-secondary shadow border-0">
                     <div class="card-body px-lg-5 py-lg-5">
                         <div class="text-center text-muted mb-4">
-                            <small>{{ __('Verify Your Email Address') }}</small>
+                            <small>{{ __('auth.verify_email') }}</small>
                         </div>
                         <div>
                             @if (session('resent'))
                                 <div class="alert alert-success" role="alert">
-                                    {{ __('A fresh verification link has been sent to your email address.') }}
+                                    {{ __('auth.verification_sent') }}
                                 </div>
                             @endif
-                            
-                            {{ __('Before proceeding, please check your email for a verification link.') }}
-                            
+
+                            {{ __('auth.Before_proceeding') }}
+
                             @if (Route::has('verification.resend'))
-                                {{ __('If you did not receive the email') }}, 
+                                {{ __('auth.not_receive_email') }},
                                 <form class="d-inline" method="POST" action="{{ route('verification.resend') }}">
                                     @csrf
-                                    <button type="submit" class="btn btn-link p-0 m-0 align-baseline">{{ __('click here to request another') }}</button>.
+                                    <button type="submit" id="request_another" class="btn btn-link p-0 m-0 align-baseline">{{ __('auth.request_another') }}</button>.
                                 </form>
                             @endif
                         </div>
